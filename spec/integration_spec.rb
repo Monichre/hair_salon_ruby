e8d9e07f2d07444f02bf3b648ea0be9523bd7607
Liam Ellis <liamhellis@gmail.com>
2016-07-15 17:06:37 -0700

    index integration spec

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,4 +1,28 @@
 require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
-set(:show_exceptions, false) 
+set(:show_exceptions, false)
+
+describe('index path', {:type => :feature}) do
+  it('displays Home page') do
+    visit('/')
+    expect(page).to have_content("Ruby Salon")
+  end
+  it('displays Home page') do
+    visit('/')
+    click_link('Meet our stylists')
+    expect(page).to have_content("There are no stylists at this time. Please contact our receptionist, thank you 555-555-5555")
+  end
+end
+
+# describe('seeing details for a single list', {:type => :feature}) do
+#   it('allows a user to click a list to see the tasks and details for it') do
+#     test_list = List.new({:name => 'School stuff'})
+#     test_list.save()
+#     test_task = Task.new({:description => "learn SQL", :list_id => test_list.id()})
+#     test_task.save()
+#     visit('/lists')
+#     click_link(test_list.name())
+#     expect(page).to have_content("Browse our current stylists")
+#   end
+# end
