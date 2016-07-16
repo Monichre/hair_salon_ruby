60f8daee8c66e083893b4822c1c9912c7c17312a
Liam Ellis <liamhellis@gmail.com>
2016-07-15 20:44:36 -0700

    database makes integration testing easier

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -15,14 +15,14 @@ describe('index path', {:type => :feature}) do
   end
 end
 
-# describe('seeing details for a single list', {:type => :feature}) do
-#   it('allows a user to click a list to see the tasks and details for it') do
-#     test_list = List.new({:name => 'School stuff'})
-#     test_list.save()
-#     test_task = Task.new({:description => "learn SQL", :list_id => test_list.id()})
-#     test_task.save()
-#     visit('/lists')
-#     click_link(test_list.name())
-#     expect(page).to have_content("Browse our current stylists")
-#   end
-# end
+describe('seeing details for a single list', {:type => :feature}) do
+  it('allows a user to click a list to see the tasks and details for it') do
+    test_stylist = Stylist.new({:name => 'Veronica', :id => nil})
+    test_stylist.save()
+    test_client = Client.new({:name => "Marve", :stylist_id => test_stylist.id()})
+    test_client.save()
+    visit('/stylists')
+    click_link(test_stylist.name())
+    expect(page).to have_content("Veronica")
+  end
+end
